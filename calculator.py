47251143041eefd53ecf06e6bd6c8b2ebb843a79
Han jisu <haneh1501@naver.com>
2022-02-15 20:58:35 +0900

    Add get_Median function

--- calculator.py
+++ calculator.py
@@ -1,17 +1,18 @@
 ```python
 # 기본계산기
 def add(a, b):
-
-​	return a+b
+	return a+b
 
 def substract(a, b):
-
-​	return a-b
+	return a-b
 
 def multiply(a, b):
     return a * b
 
 def divide_new(a, b):
     return a / b
+
+def get_Median(a, b):
+    return (a + b) / 2
 
 ```
